6cb559eb3d80d95a8cffa23bca99409323342790
diogobarrios <diogobarrios22@gmail.com>
2022-02-19 16:46:42 +0000

    update - ch3 - Functions

--- automate_boring_stuff_with_python/collatz_sequence.py
+++ automate_boring_stuff_with_python/collatz_sequence.py
@@ -1,4 +1,6 @@
 # Sequence Collatz
+
+# Collatz Function():
 def collatz(number):
     # If the number is even, number // 2
     if number % 2 == 0:
@@ -9,18 +11,21 @@ def collatz(number):
 
 
 print('type a number:')
-number = int(input())  # the first input()
-while True:
-    # Get the result from collatz() with the first input()
-    result = collatz(number)
-    # assigned the new value to the number variable
-    number = result
-    # print the new value
-    print(number)
-    # if result is not equal to 1 (True), repeat the process
-    if result != 1:
-        continue
-    # if result is equal to 1 (False), print the text in the string and break
-    # the while loop
-    print('You did it!')
-    break
+try:
+    number = int(input())  # the first input()
+    while True:
+        # Get the result from collatz() with the first input()
+        result = collatz(number)
+        # assigned the new value to the number variable
+        number = result
+        # print the new value
+        print(number)
+        # if result is not equal to 1 (True), repeat the process
+        if result != 1:
+            continue
+        # if result is equal to 1 (False), print the text in the string and break
+        # the while loop
+        print('You did it!')
+        break
+except ValueError:
+    print('Error: Invalid argument')
