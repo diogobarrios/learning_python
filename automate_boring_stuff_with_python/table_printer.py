3d239019f22fdca3f4127619d7b3147330e6df53
Diogo Barrios <diogo_barr@hotmail.com>
2022-03-26 14:52:44 +0000

    manipulating strings -pratice project

---
+++ automate_boring_stuff_with_python/table_printer.py
@@ -0,0 +1,9 @@
+# Table Printer
+
+table_data = [['apples', 'oranges', 'cherries', 'banana'],
+              ['Alice', 'Bob', 'Carol', 'David'],
+              ['dgos', 'cats', 'moose', 'goose']]
+
+# first '\n'.join()
+for string in range(len(table_data[0])):
+    print(table_data[string])
