72e7bc8bf5719ca0f608d62a1661d597beba9fe5
diogobarrios <diogobarrios22@gmail.com>
2022-04-09 20:48:04 +0100

    manipulating strings pratice project

--- automate_boring_stuff_with_python/table_printer.py
+++ automate_boring_stuff_with_python/table_printer.py
@@ -10,13 +10,18 @@ for string in range(len(table_data[0])):
     string_index.append(len(table_data[0][string]))
 longest_string = max(string_index)
 
-# Second iterate over the list with rjust() and with the length
-# of the Longest string in the list
-for string in range(len(table_data[0])):
-    print(table_data[0][string].rjust(longest_string, ' '))
-
-# third print as a table right justified
+# print each longest string from each inner list
 range_outer_list = range(len(table_data))
 range_inner_list = range(len(table_data[min(range_outer_list)]))
 print('Range of the outer list ' + str(range_outer_list) + '\n'
       'Range of the inner lists ' + str(range_inner_list))
+
+
+for list in range_outer_list:
+    string_index.append(len(table_data[list][string]))
+print(string_index)
+
+# Second iterate over the list with rjust() and with the length
+# of the Longest string in the list
+# for string in range(len(table_data[0])):
+#    print(table_data[0][string].rjust(longest_string, ' '))
