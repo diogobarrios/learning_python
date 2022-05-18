4ab34134dbfe160057cf82e24ea96e7753ad5346
Diogo Barrios <diogo_barr@hotmail.com>
2022-05-18 23:08:17 +0100

    webscrapping xkcd project

---
+++ automate_boring_stuff_with_python/xkcd.py
@@ -0,0 +1,11 @@
+#! Python3
+
+# xkcd.py - Download webcomcis from https://xkcd.com/
+
+import bs4
+import re
+import requests
+
+# re.compile(r'[^//imgs.xkcd.com/comics/]([A-Za-z0-9].+.png)')
+
+# soup.find_all('img')[2].get('src')
