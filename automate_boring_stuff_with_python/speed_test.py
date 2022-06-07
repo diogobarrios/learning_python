1428cf9758bf7b9c9f48eef052fed9cbf9ee5942
Diogo Barrios <diogo_barr@hotmail.com>
2022-06-08 00:19:26 +0100

    mini speed-test projet

--- automate_boring_stuff_with_python/speed_test.py
+++ automate_boring_stuff_with_python/speed_test.py
@@ -11,11 +11,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+import os
+from dotenv import load_dotenv
+import smtplib as smtp
+from email.mime.text import MIMEText
 
 # TODO: Marcar diariamente o teste:
 
 # Aceder ao site a fazer o teste:
-
 # url do teste:
 url = 'https://xperience.nos.pt/solution/OTT_gigaJS/'
 browser = webdriver.Chrome()
@@ -36,6 +39,7 @@ button.click()
 print("Doing test speed...")
 time.sleep(60)
 # Criação de un dicionário para captar os dados:
+dict_test = {}
 dict_test = {'id': (browser.find_element(
                 By.ID, 'testid')).get_attribute('innerHTML'),
              'Data': (browser.find_element(
@@ -52,14 +56,39 @@ dict_test = {'id': (browser.find_element(
                  By.ID, 'jitter')).get_attribute('innerHTML')
              }
 
-print("The values are...")
-for item in dict_test:
-    print("%s: %s" % (item, dict_test[item]))
 
-print("Closing...")
+with open(str(dict_test['id']) + '.txt', 'w') as f:
+    for key, value in dict_test.items():
+        f.write("%s: %s\n" % (key, value))
+    f.close()
+print("Closing the browser...")
 browser.close()
-# TODO: Enviar um mail.
 
+# Enviar um mail com a informação
+# ficheiro com a informação do teste
+txt_log = open(str(dict_test['id']) + '.txt')
+data = txt_log.read()
+
+# dados para o email
+load_dotenv()
+email_addr = os.getenv('email_address')
+email_passwd = os.getenv('email_password')
+email_sendr = 'diogobarrios22@gmail.com'
+email_subject = 'Teste de Velocidade a ' + str(dict_test['Data'])
+message = MIMEText(data)
+message['From'] = email_addr
+message['To'] = email_sendr
+message['Subject'] = email_subject
+print("Connecting with the server using SSL...")
+time.sleep(2)
+# Conectar ao servidor
+server = smtp.SMTP_SSL('smtp.gmail.com', 465)
+con = server.login(email_addr, email_passwd)
+print("Sending mail...")
+send_mail = server.sendmail(email_addr, email_sendr, message.as_string())
+print("Closing connection with the server...")
+time.sleep(2)
+server.quit()
 # TODO: Criar uma tabela onde o dicionário vai adicionando os dados
 # TODO: Ligar ao Grafana para criar um dashboard com
 # limite sup, inf, e média. (Quality Control)
