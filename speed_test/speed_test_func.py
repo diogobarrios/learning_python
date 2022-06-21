f48cca1227f7717f6e18f03b1cb6f3aaf8119895
diogobarrios <diogobarrios22@gmail.com>
2022-06-21 21:04:35 +0100

    mini project functions script in macos

---
+++ speed_test/speed_test_func.py
@@ -0,0 +1,138 @@
+#! python3
+
+# speed_test.py - fazer um teste de velocidade no site {url}
+# diariamente e receber um mail.
+
+# Confirmar o contratualizado:
+# Minimo 400Mbps / 80Mbps
+# Média  475Mbps / 95Mpbs
+# Máxima 500Mbps / 100Mbps
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import glob  # usar para encontrar ficheiros num dir.
+import os
+from dotenv import load_dotenv
+import smtplib as smtp
+from email.mime.text import MIMEText
+import sqlite3
+
+
+def test_access(isp):
+    """Accessing to internet service provider (isp)"""
+    if isp.lower() == 'nos':
+        try:
+            url = 'https://xperience.nos.pt/solution/OTT_gigaJS/'
+            browser = webdriver.Chrome()
+            browser.get(url)
+            time.sleep(30)
+
+            start_simulated = browser.find_element(By.ID, 'startButton')
+            start_simulated.click()
+            time.sleep(5)
+
+            check_button = browser.find_element(By.TAG_NAME, 'label')
+            check_button.click()
+            time.sleep(5)
+
+            start_button = browser.find_element(By.ID, 'startButton')
+            start_button.click()
+            print("Starting speed test...")
+            time.sleep(80)
+        except webdriver.Error as error:
+            print("Failed to start chromeless!")
+    else:
+        None  # it will have another isp here!
+
+
+test_access(input('(nos/meo/vodafone): '))
+
+
+# Criação de un dicionário para captar os dados:
+dict_test = {}
+dict_test = {'id': (browser.find_element(
+    By.ID, 'testid')).get_attribute('innerHTML'),
+    'Data': (browser.find_element(
+        By.ID, 'data')).get_attribute('innerHTML'),
+    'Browser': (browser.find_element(
+        By.ID, 'browser')).get_attribute('innerHTML'),
+    'Download': (browser.find_element(
+                 By.ID, 'dl')).get_attribute('innerHTML'),
+    'Upload': (browser.find_element(
+        By.ID, 'ul')).get_attribute('innerHTML'),
+    'Ping': (browser.find_element(
+        By.ID, 'ping')).get_attribute('innerHTML'),
+    'Jitter': (browser.find_element(
+        By.ID, 'jitter')).get_attribute('innerHTML')
+}
+time.sleep(30)
+with open(str(dict_test['id']) + '.txt', 'w') as f:
+    for key, value in dict_test.items():
+        f.write("%s: %s\n" % (key, value))
+    f.close()
+print("Closing the browser...")
+browser.close()
+
+# Enviar um mail com a informação se for abaixo do minimo 400Mbps / 80Mbps
+if float(dict_test['Download']) < 400.00 or float(dict_test['Upload']) < 80.00:
+    # ficheiro com a informação do teste
+    txt_log = open(str(dict_test['id']) + '.txt')
+    data = txt_log.read()
+    # dados para o email
+    load_dotenv()
+    email_addr = os.getenv('email_address')
+    email_passwd = os.getenv('email_password')
+    email_sendr = 'diogobarrios22@gmail.com'
+    email_subject = 'Teste de Velocidade a ' + str(dict_test['Data'])
+    message = MIMEText(data)
+    message['From'] = email_addr
+    message['To'] = email_sendr
+    message['Subject'] = email_subject
+    print("Connecting with the server using SSL...")
+    time.sleep(2)
+    # Conectar ao servidor
+    server = smtp.SMTP_SSL('smtp.gmail.com', 465)
+    con = server.login(email_addr, email_passwd)
+    print("Sending mail...")
+    send_mail = server.sendmail(email_addr, email_sendr, message.as_string())
+    print("Closing connection with the server...")
+    time.sleep(2)
+    server.quit()
+else:
+    None
+
+# Remove .txt file created to send mail
+remove_tmp_files = glob.glob('*.txt')
+for tmp in remove_tmp_files:
+    os.remove(tmp)
+
+
+def insert_values(id, data, browser, download, upload, ping, jitter):
+    try:
+        conn = sqlite3.connect('speed_test.db')
+        c = conn.cursor()
+        print('Connected to SQLite')
+
+        sqlite_insert_param = """INSERT INTO internet_speed
+                            (id, data, browser, download, upload, ping, jitter)
+                            VALUES (?, ?, ?, ?, ?, ?, ?);"""
+
+        test_speed_data = (id, data, browser, download, upload, ping, jitter)
+        c.execute(sqlite_insert_param, test_speed_data)
+        conn.commit()
+        print(f'Data from speed test done at {data} inserted successfully!')
+
+        conn.close()
+
+    except sqlite3.Error as error:
+        print("Failed to insert data from speed test into internet_speed table", error)
+    finally:
+        if conn:
+            conn.close()
+            print("The SQLite connection is closed")
+
+
+insert_values(dict_test["id"], dict_test["Data"], dict_test["Browser"],
+              dict_test["Download"], dict_test["Upload"], dict_test["Ping"],
+              dict_test["Jitter"])
