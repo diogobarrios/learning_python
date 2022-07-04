20b2f75c53e665e281954f49e9e504ab5d552402
diogobarrios <diogobarrios22@gmail.com>
2022-07-04 20:45:23 +0100

    python crash course - files and exceptions

--- python_crash_course/file_reader.py
+++ python_crash_course/file_reader.py
@@ -1,10 +1,18 @@
-filename = 'pi_digits.txt'
+filename = 'pi_million_digits.txt'
 
 with open(filename) as file_object:  # with will close() the file properly when python have to
-    lines = file_object.readlines()
+    lines = file_object.readlines()  # will transform in a list
 
 pi_string = ''
-for line in lines:  # this will create a list which can be work later
-    pi_string += line.rstrip()  # rstrip() removes the blank line below the result
+for line in lines:
+    # pi_string += line.rstrip()  # rstrip() removes the blank line below the result
+    pi_string += line.strip()  # removes all blanks spaces
 
-print(pi_string)
+birthday = input("Enter your birthday, in the form mmddyy: ")
+if birthday in pi_string:
+    print("Your birthday appears in the first million digits of pi!")
+else:
+    print("Your birthday does not appear in the first million digits of pi")
+# print(pi_string)  # this will give me 32 digits
+# print(f"{pi_string[:52]}...")  # If I want only the first 52 results
+# print(len(pi_string))
